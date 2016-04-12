33ff83a2911ae6367d97b244688f7f630675ed36
HomHomLin <linhh90@163.com>
2016-04-12 13:28:12 +0800

    Initial commit

---
+++ app/src/main/java/lib/homhomlib/design/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package lib.homhomlib.design.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lib.homhomlib.design.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
