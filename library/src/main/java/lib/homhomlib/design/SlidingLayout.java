7ebd382ab112a11c087b9c3c9abd27d6ae4e2993
HomHomLin <linhh90@163.com>
2016-04-13 11:36:04 +0800

    兼容各种view

--- library/src/main/java/lib/homhomlib/design/SlidingLayout.java
+++ library/src/main/java/lib/homhomlib/design/SlidingLayout.java
@@ -1,10 +1,15 @@
 package lib.homhomlib.design;
 
 import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.webkit.WebView;
+import android.widget.AbsListView;
 import android.widget.FrameLayout;
 
 import java.util.ArrayList;
@@ -14,14 +19,20 @@ import java.util.ArrayList;
  */
 public class SlidingLayout extends FrameLayout{
 
+    private int mTouchSlop;//系统允许最小的滑动判断值
+
     private View mViewBack;//背景View
     private View mViewFront;//正面View
 
     private static final int UN_SLIDING = 0;
 
-    private int mLastSlidingY;
+    private boolean mIsBeingDragged;
+    private float mInitialDownY;
+    private float mInitialMotionY;
+    private int mActivePointerId = INVALID_POINTER;
 
-    private long mLastSlidingTime = 0;
+
+    private static final int INVALID_POINTER = -1;
 
     public SlidingLayout(Context context) {
         this(context, null);
@@ -37,7 +48,7 @@ public class SlidingLayout extends FrameLayout{
     }
 
     private void init(){
-        mLastSlidingY = UN_SLIDING;
+        mTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
     }
 
     public void setBackView(View view){
@@ -53,8 +64,77 @@ public class SlidingLayout extends FrameLayout{
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
 
+        final int action = MotionEventCompat.getActionMasked(ev);
+
         //判断拦截
-        return true;
+
+        //子控件能否滑动
+        if (canChildScrollUp()) {
+            return false;
+        }
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mActivePointerId = MotionEventCompat.getPointerId(ev, 0);
+                mIsBeingDragged = false;
+                final float initialDownY = getMotionEventY(ev, mActivePointerId);
+                if (initialDownY == -1) {
+                    return false;
+                }
+                mInitialDownY = initialDownY;
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                if (mActivePointerId == INVALID_POINTER) {
+//                    Log.e(LOG_TAG, "Got ACTION_MOVE event but don't have an active pointer id.");
+                    return false;
+                }
+
+                final float y = getMotionEventY(ev, mActivePointerId);
+                if (y == -1) {
+                    return false;
+                }
+                final float yDiff = y - mInitialDownY;
+                if (yDiff > mTouchSlop && !mIsBeingDragged) {
+                    mInitialMotionY = mInitialDownY + mTouchSlop;
+                    mIsBeingDragged = true;
+                }
+                break;
+
+            case MotionEventCompat.ACTION_POINTER_UP:
+                break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mIsBeingDragged = false;
+                mActivePointerId = INVALID_POINTER;
+                break;
+        }
+
+        return mIsBeingDragged;
+    }
+
+    private float getMotionEventY(MotionEvent ev, int activePointerId) {
+        final int index = MotionEventCompat.findPointerIndex(ev, activePointerId);
+        if (index < 0) {
+            return -1;
+        }
+        return MotionEventCompat.getY(ev, index);
+    }
+
+    public boolean canChildScrollUp() {
+        if (android.os.Build.VERSION.SDK_INT < 14) {
+            if (mViewFront instanceof AbsListView) {
+                final AbsListView absListView = (AbsListView) mViewFront;
+                return absListView.getChildCount() > 0
+                        && (absListView.getFirstVisiblePosition() > 0 || absListView.getChildAt(0)
+                        .getTop() < absListView.getPaddingTop());
+            } else {
+                return ViewCompat.canScrollVertically(mViewFront, -1) || mViewFront.getScrollY() > 0;
+            }
+        } else {
+            return ViewCompat.canScrollVertically(mViewFront, -1);
+        }
     }
 
     @Override
@@ -67,22 +147,21 @@ public class SlidingLayout extends FrameLayout{
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                mLastSlidingY = (int) event.getY();
                 break;
             case MotionEvent.ACTION_MOVE:
                 Log.i("onTouchEvent", "move");
-                float delta = (event.getY() - mLastSlidingY) / 3.0F;
-                if(delta > 0 ){
-                    //向下滑动
-                    Instrument.getInstance().slidingByDelta(mViewFront, delta);
-                }else{
-                    //向上滑动
-                }
+                float delta = (event.getY() - mInitialMotionY) / 3.0F;
+                Instrument.getInstance().slidingByDelta(mViewFront, delta);
+//                if(delta > 0 ){
+//                    //向下滑动
+//                    Instrument.getInstance().slidingByDelta(mViewFront, delta);
+//                }else{
+//                    //向上滑动
+//                }
 
                 break;
             case MotionEvent.ACTION_UP:
-                mLastSlidingY = UN_SLIDING;
-                mLastSlidingTime = 0;
+            case MotionEvent.ACTION_CANCEL:
                 Instrument.getInstance().slidingToY(mViewFront,0);
                 break;
         }
